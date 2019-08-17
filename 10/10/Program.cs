700704e7b47caa835075214ef21121b6b6b273ed
Ilgaz <ilgaz@altinoren.com>
2019-08-17 08:37:29 +0300

    Two new problems. Problem 9 couldn't be generelized and Problem 10 is just a tweaked version of Problem 7.

---
+++ 10/10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10
+{
+    class Program
+    {
+        static long FindPrimeSum(int limit)
+        {
+            long current_sum = 2;
+            IList<int> primeList = new List<int>{2};
+            int current = 3;
+            while (current < limit)
+            {
+                bool isPrime = true;
+                foreach (int prime in primeList)
+                {
+                    if (current % prime == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primeList.Add(current);
+                    current_sum += current;
+                }
+                current+=2;
+
+            }
+            return current_sum;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindPrimeSum(2000000));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 9/9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool found = false;
+            while(found == false)
+            {
+                for(double a = 1; a <= 333; a++)
+                {
+                    for(double b = a; b < (1000 - a)/2 && b < a*a/2; b++)
+                    {
+                        double c = Math.Sqrt(a * a + b * b);
+                        if (c == Math.Round(c , 0))
+                        {
+                            if (a + b + c == 1000)
+                            {
+                                found = true;
+                                Console.WriteLine(a * b * c);
+                                break;
+                            }
+                        }
+                    }
+                    if(found)
+                    {
+                        break;
+                    }
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
